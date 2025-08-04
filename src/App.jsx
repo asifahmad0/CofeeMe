80e06b7320dbaadfb49843808dea264943a50617
asif7649 <asifahma7761@gmail.com>
2025-08-05 01:24:25 +0530

    cofee me complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Hero from './componant/Hero'
+import Services from './componant/Services'
+import Banner from './componant/Banner'
+import AppStore from './componant/AppStore'
+import Testimonials from './componant/Testimonials'
+import Footer from './componant/Footer'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+  
 
 function App() {
+
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   const [count, setCount] = useState(0)
 
   return (
@@ -9,6 +28,12 @@ function App() {
      <div className=' overflow-x-hidden'>
       
       <Hero/>
+      <Services/>
+      <Banner/>
+      <AppStore/>
+      <Testimonials/>
+      <Footer/>
+
      </div>
     </>
   )
